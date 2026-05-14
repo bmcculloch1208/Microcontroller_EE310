1d52781a5c0355d9b0101c571d290ad85cd4a8fb
Brandon McCulloch <brandon@bmm.solutions>
2026-05-14 11:25:23 -0700

    Lab13

---
+++ Lab/Lab13/main.c
@@ -0,0 +1,120 @@
+/*
+ * The purpose of this program is demonstrate INT0 as an external interrupt. 
+ * In this case when the signal changes state on RB0 (INT0) then D0 starts 
+ * blinking for 4 seconds and then stops. The results can be simulated and
+ * verified.  
+ * 
+ * Author: Farid Farahmand 
+ */
+
+
+// PIC18F46K42 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1L
+#pragma config FEXTOSC = LP     // External Oscillator Selection (LP (crystal oscillator) optimized for 32.768 kHz; PFM set to low power)
+#pragma config RSTOSC = EXTOSC  // Reset Oscillator Selection (EXTOSC operating per FEXTOSC bits (device manufacturing default))
+
+// CONFIG1H
+#pragma config CLKOUTEN = OFF   // Clock out Enable bit (CLKOUT function is disabled)
+#pragma config PR1WAY = ON      // PRLOCKED One-Way Set Enable bit (PRLOCK bit can be cleared and set only once)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+
+// CONFIG2L
+#pragma config MCLRE = EXTMCLR  // MCLR Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
+#pragma config PWRTS = PWRT_OFF // Power-up timer selection bits (PWRT is disabled)
+#pragma config MVECEN = ON      // Multi-vector enable bit (Multi-vector enabled, Vector table used for interrupts)
+#pragma config IVT1WAY = ON     // IVTLOCK bit One-way set enable bit (IVTLOCK bit can be cleared and set only once)
+#pragma config LPBOREN = OFF    // Low Power BOR Enable bit (ULPBOR disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
+
+// CONFIG2H
+#pragma config BORV = VBOR_2P45 // Brown-out Reset Voltage Selection bits (Brown-out Reset Voltage (VBOR) set to 2.45V)
+#pragma config ZCD = OFF        // ZCD Disable bit (ZCD disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config DEBUG = OFF      // Debugger Enable bit (Background debugger disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode disabled)
+
+// CONFIG3L
+#pragma config WDTCPS = WDTCPS_31// WDT Period selection bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = ON       // WDT operating mode (WDT Disabled; SWDTEN is ignored)
+
+// CONFIG3H
+#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+
+// CONFIG4L
+#pragma config BBSIZE = BBSIZE_512// Boot Block Size selection bits (Boot Block size is 512 words)
+#pragma config BBEN = OFF       // Boot Block enable bit (Boot block disabled)
+#pragma config SAFEN = OFF      // Storage Area Flash enable bit (SAF disabled)
+#pragma config WRTAPP = OFF     // Application Block write protection bit (Application Block not write protected)
+
+// CONFIG4H
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block not write-protected)
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+#pragma config WRTSAF = OFF     // SAF Write protection bit (SAF not Write Protected)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
+
+// CONFIG5L
+#pragma config CP = OFF         // PFM and Data EEPROM Code Protection bit (PFM and Data EEPROM code protection disabled)
+
+#include <xc.h> // must have this
+#include "MyConfigFile.h"
+
+//#include "../../../../../Program Files/Microchip/xc8/v2.40/pic/include/proc/pic18f46k42.h"
+//#include "C:\Program Files\Microchip\xc8\v2.40\pic\include\proc\pic18f46k42"
+
+#define _XTAL_FREQ 4000000UL
+
+#define OUTPUT 0
+
+#define BlinkLED_LAT   LATDbits.LATD0
+#define WdtLED_LAT     LATDbits.LATD1
+#define PORTD_DIR      TRISD
+
+unsigned int count;
+
+void Dummy_Stuck(void)
+{
+    while(1)
+    {
+        // Intentionally do nothing.
+        // No CLRWDT, WDT will eventually reset PIC.
+    }
+}
+
+void main(void)
+{
+    // Initialization
+    ANSELD = 0x00;          // PORTD digital
+    PORTD_DIR = OUTPUT;     // All PORTD pins as outputs
+    LATD = 0x00;            // Initialize PORTD outputs low
+
+    // Optional WDT register setting from your original code
+    // Keep only if this matches your intended WDT postscaler setup
+    //WDTCON0 = 0b00001011;
+
+    __delay_ms(1000);
+
+    count = 0;
+    BlinkLED_LAT = 0;
+    WdtLED_LAT = 0;
+
+    while(1) {
+        BlinkLED_LAT = !BlinkLED_LAT;
+        __delay_ms(100);
+        CLRWDT();
+        count++;
+
+        if(count > 5)
+        {
+            WdtLED_LAT = 1;
+            Dummy_Stuck();
+        }
+    }
+}
+
