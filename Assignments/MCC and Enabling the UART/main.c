d4c7ff39a7a3ae349128b09dc434db1cf49da145
Brandon McCulloch <brandon@bmm.solutions>
2026-05-14 20:24:53 -0700

    Update main.c

--- Assignments/MCC and Enabling the UART/main.c
+++ Assignments/MCC and Enabling the UART/main.c
@@ -1,3 +1,40 @@
+/*
+;---------------------------------------------------------------
+; Title: Interfacing with PIC18 via Serial
+;---------------------------------------------------------------
+; Program Details:
+;   This program controls a servo using two buttons. If the first button is pressed, then the servo
+;   turns left. When the 2nd button is pressed, the servo moves right.
+;   When no buttons are pressed, the servo holds its position for a few seconds. 
+;   The servo stops when it reaches it limit in either direction.
+;
+; Inputs:
+;
+; Outputs:
+;   USB Connection
+;
+; Setup:
+;   - PIC18F47K42 mounted on breadboard
+;   - Two pushbuttons used as servo directional input
+;   - One servo connected as an output
+;
+; Date:
+;   May 13, 2026
+;
+; File Dependencies / Libraries:
+;   - mcc_generated_files/system/system.h
+;
+; Compiler:
+;   XC8 v3.10
+;
+; Author:
+;   Brandon McCulloch
+;
+; Versions:
+;   V1.0
+;---------------------------------------------------------------
+*/
+
 #include "mcc_generated_files/system/system.h"
 #include <stdio.h>
 #include <time.h>
